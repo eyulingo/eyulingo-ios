a697bb59b0db07022abb5e10689fdc5d29d685e8
yuetsin <akaza_akari@sjtu.edu.cn>
2019-07-08 16:04:33 +0800

    [fix] change uris

---
+++ iOS/eyulingo/Eyulingo/Utils/EyulingoUri.swift
@@ -0,0 +1,17 @@
+//
+//  EyulingoUri.swift
+//  Eyulingo
+//
+//  Created by 法好 on 2019/7/1.
+//  Copyright © 2019 yuetsin. All rights reserved.
+//
+
+import Foundation
+
+class EyulingoUri {
+    // parameters:
+    // username - username
+    // password - password (废话)
+    static let loginPostUri = "http://47.103.15.32:8080/login"
+    
+}
