316d7ee0c5c17dbffbd3cfcaa0e57beb43cbc9f2
yuetsin <akaza_akari@sjtu.edu.cn>
2019-07-09 08:53:10 +0800

    [add] Api design

--- iOS/eyulingo/Eyulingo/Utils/DateAndTimeParser.swift
+++ iOS/eyulingo/Eyulingo/Utils/DateAndTimeParser.swift
@@ -1,6 +1,6 @@
 //
 //  DateAndTimeParser.swift
-//  
+//
 //
 //  Created by 法好 on 2019/7/2.
 //
@@ -14,7 +14,7 @@ class DateAndTimeParser {
         // 2019-07-02 00:20:10.0
         dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss.S"
         betterFormatter.dateFormat = "yyyy 年 MM 月 dd 日 HH:mm:ss"
-    
+
         if var date = dateFormatter.date(from: str) {
             if convert {
                 date = date.addingTimeInterval(TimeInterval(60 * 60 * 8))
@@ -23,7 +23,7 @@ class DateAndTimeParser {
         }
         return str
     }
-    
+
     static func getDateFromString(_ str: String, makeUTC8Conversion convert: Bool = true) -> Date? {
         let dateFormatter = DateFormatter()
         // 2019-07-02 00:20:10.0
@@ -37,14 +37,14 @@ class DateAndTimeParser {
         }
         return nil
     }
-    
+
     static func parseDayFromString(_ str: String, makeUTC8Conversion convert: Bool = true) -> String {
         let dateFormatter = DateFormatter()
         let betterFormatter = DateFormatter()
         // 2019-07-02 00:20:10.0
         dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss.S"
         betterFormatter.dateFormat = "yyyy 年 MM 月 dd 日"
-        
+
         if var date = dateFormatter.date(from: str) {
             if convert {
                 date = date.addingTimeInterval(TimeInterval(60 * 60 * 8))
--- iOS/eyulingo/Eyulingo/Utils/EyulingoUri.swift
+++ iOS/eyulingo/Eyulingo/Utils/EyulingoUri.swift
@@ -8,8 +8,8 @@
 
 import Foundation
 
-class EyulingoUri {
-    static let baseUri = "http://47.103.15.32:8080"
+class Eyulingo_UserUri {
+    fileprivate static let baseUri = "http://47.103.15.32:8080"
     // parameters:
     // username - username
     // password - password (废话)
@@ -17,16 +17,96 @@ class EyulingoUri {
     static let loginPostUri = baseUri + "/login"
     
     // parameters:
-    // phone_nu - 手机号
-    static let captchaGetPostUri = baseUri +  "/getcode"
+    // email - 电子邮箱
+    static let captchaGetPostUri = baseUri + "/getcode"
     
     // parameters:
-    // “phone_nu” - 手机号码
-    // “username” - 用户名
-    // “password” - 密码
-    // “confirm_password” - 确认密码
-    // “confirm_code” - 验证码
-    static let registerPostUri = "/register"
+    // email - 电子邮箱
+    // username - 用户名
+    // password - 密码
+    // confirm_password - 确认密码
+    // confirm_code - 验证码
+    static let registerPostUri = baseUri + "/register"
     
-    static let profileGetUri = "/"
+    // parameters: <none>
+    static let profileGetUri = baseUri + "/myprofile"
+    
+    // parameters:
+    // avatar_id: <MongoDB 图片 ID>
+    static let avatarPostUri = baseUri + "/setavatar"
+    
+    // parameters:
+    // origin_password - 旧密码
+    // new_password - 新密码
+    // confirm_new_password - 确认新密码
+
+    static let passwordChangePostUri = baseUri + "/changepassword"
+    
+    // parameters:
+    // new_email - 新邮箱
+    // confirm_code - 确认代码
+    static let emailChangePostUri = baseUri + "/changeemail"
+    
+    // parameters: <none>
+    static let addressGetUri = baseUri + "/myaddress"
+    
+    // parameters:
+    // receive_no - 收货人
+    // receive_phone - 收货电话
+    // receive_address - 收货地址
+    static let addAddressPostUri = baseUri + "/addaddress"
+    
+    // parameters:
+    // receive_no - 收货人
+    // receive_phone - 收货电话
+    // receive_address - 收货地址
+    static let removeAddressPostUri = baseUri + "/removeaddress"
+    
+    // parameters:
+    // q - 关键字
+    static let searchGoodsGetUri = baseUri + "/searchgood"
+    
+    // parameters:
+    // q - 关键字
+    static let searchStoresGetUri = baseUri + "/searchstore"
+    
+    // parameters:
+    // id - 商品 ID
+    static let goodDetailGetUri = baseUri + "/gooddetail"
+    
+    // parameters:
+    // id - 店铺 ID
+    static let storeDetailGetUri = baseUri + "/storedetail"
+    
+    // parameters:
+    // id - 商品 ID
+    // amount - 购买数量
+    static let addToCartPostUri = baseUri + "/addtocart"
+    
+    // parameters: <none>
+    static let myCartGetUri = baseUri + "/mycart"
+    
+    // parameters:
+    // id - 商品 ID
+    // amount - 商品数量
+    // 附注：同时购买多个商品时，ID 和 amount 出现多次。
+    // receive_no - 收货人
+    // receive_phone - 收货电话
+    // receive_address - 收货地址
+    static let purchasePostUri = baseUri + "/purchase"
+    
+    // parameters:
+    // id - 商品 ID
+    // star_count - 星级 (1, 2, 3, 4, 5)
+    // comment_content - 评价内容
+    static let commentGoodsPostUri = baseUri + "/commentgood"
+    
+    // parameters:
+    // id - 商店 ID
+    // star_count - 星级 (1, 2, 3, 4, 5)
+    // comment_content - 评价内容
+    static let commentStorePostUri = baseUri + "/commentstore"
+    
+    // parameters: <none>
+    static let purchasedGetUri = baseUri + "/orderlist"
 }
